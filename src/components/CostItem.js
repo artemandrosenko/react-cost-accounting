d92fdc23e63a72c291fc21754cb1b14ada56b472
Artem Androsenko <tyoma.and@gmail.com>
2023-05-20 20:44:36 +0300

    Add CostItem component and style for it

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,5 +1,15 @@
+import './CostItem.css';
+
 function CostItem() {
-    return <h2>Элемент расхода</h2>
+    return (
+        <div className='cost-item'>
+            <div>March 12 2022</div>
+            <div className='cost-item__description'>
+                <h2>Refrigerator</h2>
+                <div className='cost-item__price'>$999.99</div>
+            </div>
+        </div>
+    )
 }
 
 export default CostItem;
