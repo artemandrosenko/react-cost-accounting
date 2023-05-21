3571a679292498617faa31ba99cf52d7c131ac5c
Artem Androsenko <tyoma.and@gmail.com>
2023-05-21 10:16:00 +0300

    Add dynamic data for component

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,12 +1,16 @@
 import './CostItem.css';
 
 function CostItem() {
+    const costDate = new Date(2022, 2, 12);
+    const costName = 'Refrigerator';
+    const costAmount = 999.99;
+    
     return (
         <div className='cost-item'>
-            <div>March 12 2022</div>
+            <div>{costDate.toISOString()}</div>
             <div className='cost-item__description'>
-                <h2>Refrigerator</h2>
-                <div className='cost-item__price'>$999.99</div>
+                <h2>{costName}</h2>
+                <div className='cost-item__price'>${costAmount}</div>
             </div>
         </div>
     )
