5a8fa83d4b6f1e0f3a7f528e98492cdc31550cd4
Artem Androsenko <tyoma.and@gmail.com>
2023-05-22 13:55:56 +0300

    Add CostDate component with style

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,4 +1,5 @@
 import './CostItem.css';
+import CostDate from './CostDate';
 
 function CostItem(props) {
     // const costDate = new Date(2022, 2, 12);
@@ -8,7 +9,7 @@ function CostItem(props) {
     return (
         <div className='cost-item'>
             {/* <div>{costDate.toISOString()}</div> */}
-            <div>{(props.date).toISOString()}</div>
+            <CostDate date={props.date} />
             <div className='cost-item__description'>
                 <h2>{props.description}</h2>
                 <div className='cost-item__price'>${props.amount}</div>
