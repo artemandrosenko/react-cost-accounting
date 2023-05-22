1ff3d0a4a0bd443e2a45bcf335d0b4fc2dfefcb4
Artem Androsenko <tyoma.and@gmail.com>
2023-05-22 23:21:49 +0300

    Composition (props.children)

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,5 +1,6 @@
 import './CostItem.css';
 import CostDate from './CostDate';
+import Card from './Card';
 
 function CostItem(props) {
     // const costDate = new Date(2022, 2, 12);
@@ -7,14 +8,14 @@ function CostItem(props) {
     // const costAmount = 999.99;
     
     return (
-        <div className='cost-item'>
+        <Card className='cost-item'>
             {/* <div>{costDate.toISOString()}</div> */}
             <CostDate date={props.date} />
             <div className='cost-item__description'>
                 <h2>{props.description}</h2>
                 <div className='cost-item__price'>${props.amount}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
