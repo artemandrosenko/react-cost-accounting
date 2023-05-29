e6e160a32715a247935fa64c60ebc5f845b6bd4a
Artem Androsenko <tyoma.and@gmail.com>
2023-05-29 10:59:42 +0300

    Add dynamic list in Costs component

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -2,21 +2,28 @@ import CostItem from "./CostItem";
 import Card from "../UI/Card";
 import CostsFilter from "./CostsFilter";
 import "./Costs.css";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function Costs(props) {
-    const [selectedYear, setSelectedYear] = useState('2021');
+  const [selectedYear, setSelectedYear] = useState("2022");
 
-    const yearChangeHandler = (year) => {
-        console.log(year);
-        setSelectedYear(year);
-    }
+  const yearChangeHandler = (year) => {
+    console.log(year);
+    setSelectedYear(year);
+  };
 
   return (
     <div>
       <Card className="costs">
-      <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler}/>
-        <CostItem
+        <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
+        {props.costs.map((cost) => (
+          <CostItem
+            date={cost.date}
+            description={cost.description}
+            amount={cost.amount}
+          />
+        ))}
+        {/* <CostItem
           date={props.costs[0].date}
           description={props.costs[0].description}
           amount={props.costs[0].amount}
@@ -30,7 +37,7 @@ function Costs(props) {
           date={props.costs[2].date}
           description={props.costs[2].description}
           amount={props.costs[2].amount}
-        />
+        /> */}
       </Card>
     </div>
   );
